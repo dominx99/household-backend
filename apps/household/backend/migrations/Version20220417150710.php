16c24f6d76e4fe170efa73990bc354cb9ff1d243
dominx99 <dominx992@gmail.com>
2022-04-18 07:07:23 +0200

    Remove duty variant context

--- apps/household/backend/migrations/Version20220417150710.php
+++ apps/household/backend/migrations/Version20220417150710.php
@@ -11,13 +11,11 @@ final class Version20220417150710 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE duty_variants (id VARCHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, points VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tasks (id VARCHAR(36) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE duty_variants');
         $this->addSql('DROP TABLE tasks');
     }
 }
